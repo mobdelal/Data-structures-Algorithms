d1abea1981fe65c50d075f4586badfd3d79b007c
Mohammed Abdelal <mobdelal@gmail.com>
2025-01-06 12:06:16 +0200

    Bubble Sort

---
+++ BubbleSort/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        // Bubble sort with optimization so it dosn't make unnessary loops if the array is sorted 
+        static void Main(string[] args)
+        {
+            int[] ints = { 3, 15, 8, 12, 9, 1 };
+            BubbleSort(ints);
+
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int[] BubbleSort(int[] arr)
+        {
+            bool swaps;
+            for (int i = arr.Length; i > 0; i--)
+            {
+                swaps = true;
+                for (int j = 0; j < i -1; j++)
+                {
+                    if (arr[j] > arr[j+ 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j+ 1];
+                        arr[j+ 1] = temp;
+                        swaps = false;
+                    }
+                }
+                if (swaps)
+                {
+                    break;
+                }
+            }
+            return arr;
+        }
+    }
+}
